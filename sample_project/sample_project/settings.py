a826b60db91f4547b322f610038d2664d5da0685
nakama_Git <omegafx777@gmail.com>
2021-05-03 11:27:54 +0900

    update : settings.pyにtempleteディレクトリの記述を追加

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sample_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templete'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
