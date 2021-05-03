c0e6039c5cb23cd15ca1912261a9b77de996c017
nakama_Git <omegafx777@gmail.com>
2021-05-03 12:41:43 +0900

    update : バックエンドにてaccountのルーティング取得ができるよう編集

--- sample_project/account/views.py
+++ sample_project/account/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 def index(request):
     return HttpResponse(200)
+
+url = reverse('account:index')
